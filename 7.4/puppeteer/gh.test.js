e6b7b450b6d7215912f9474fe0ed81ac3d758623
Leferchian <leferchian@gmail.com>
2023-04-04 18:56:40 +0300

    initial commit

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,17 +9,22 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("http://github.com/team")
+  }, 60000);
+
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
+    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub', {timeout: 60000});
   });
 
   test("The first link attribute", async () => {
     const actual = await page.$eval("a", link => link.getAttribute('href') );
-    expect(actual).toEqual("#start-of-content");
+    expect(actual).toEqual("#start-of-content", {timeout: 60000});
   });
 
   test("The page contains Sign in button", async () => {
@@ -29,6 +33,30 @@ describe("Github page tests", () => {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
+    expect(actual).toContain("Get started with Team", {timeout: 60000})
+  });
+
+  describe("Should check titles for GitHub pages", () => {
+    test("Should check Pricing page", async () => {
+      await page.goto("https://github.com/pricing");
+      const title = await page.title();
+      expect(title).toContain("Pricing · Plans for every developer · GitHub")
+    }, 60000);
+  
+    test("Should check Shop page", async () => {
+      await page.goto("https://github.com/marketplace");
+      const title = await page.title();
+      expect(title).toContain("GitHub Marketplace · to improve your workflow · GitHub")
+    }, 60000);
+
+    test("Should check Skill page", async () => {
+      await page.goto("https://skills.github.com/");
+      await page.$("header div div a");
+      await page.waitForSelector("h1");
+      const title2 = await page.title();
+      expect(title2).toEqual("GitHub Skills");
+    }, 60000);
+  
+
   });
 });
